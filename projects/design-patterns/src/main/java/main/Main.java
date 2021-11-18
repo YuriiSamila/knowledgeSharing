7bc17d7c12a1a63fb0461e7ff50711a9a911ab6d
YuriiSamila <yurii.samila@logivations.com>
2021-11-18 10:43:35 +0200

    Init commit

---
+++ projects/design-patterns/src/main/java/main/Main.java
@@ -0,0 +1,11 @@
+package main;
+
+import patterns.TestPattern;
+
+public class Main {
+
+    public static void main(String[] args) {
+        TestPattern testPattern = new TestPattern();
+    }
+
+}
